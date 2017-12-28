6f3c208781a32a54d4b61a5537bb4b639f828445
haykam821 <haykam821@users.noreply.github.com>
2017-12-28 16:46:20 -0500

    Trying a ternary operator
    But now there is over 80 pixels of a gap between rows

--- client.js
+++ client.js
@@ -690,7 +690,7 @@ function drawUpgrades() {
 	if (selfPlayer.tier || [undefined, null, {}, []].contains(selfTankUpgrades)) {
 		for (let index = 0; index < Object.keys(selfTankUpgrades).length; index++) {
 			let slotX = 10 + 86.25 * (index % 2);
-			let slotY = 60 + 86.25 * index;
+			let slotY = index % 2 === 1 ? 60 + 86.25 * (index - 1) : 60 + 86.25 * index;
 
 			if (nfup(index) !== undefined && stfup(index)) {
 				drawUpgradeSlot(slotX, slotY, 80, 80, '#6cf1ec', nfup(index));
