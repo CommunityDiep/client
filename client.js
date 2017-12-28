5dd4f8628b749c9b2210b5497c08c60390022de8
haykam821 <haykam821@users.noreply.github.com>
2017-12-28 16:55:34 -0500

    Add snarky comment

--- client.js
+++ client.js
@@ -687,7 +687,7 @@ function drawUpgrades() {
 		return Object.values(selfTankUpgrades)[pos] <= selfPlayer.tier;
 	}
 
-	if (selfPlayer.tier || [undefined, null, {}, []].includes(selfTankUpgrades)) {
+	if (selfPlayer.tier || [undefined, null, {}, []].includes(selfTankUpgrades)) { // whenever I have to change this line again for a wrong function, call it a METHOD so RBT doesn't yell at me
 		for (let index = 0; index < Object.keys(selfTankUpgrades).length; index++) {
 			let slotX = 10 + 86.25 * (index % 2);
 			let slotY = index % 2 === 1 ? 60 + 86.25 * (index - 1) : 60 + 86.25 * index;
