e2368c7b350df5acffc3b0976098c83d81375908
haykam821 <haykam821@users.noreply.github.com>
2018-01-19 14:56:29 -0500

    Defaults function

--- client.js
+++ client.js
@@ -68,6 +68,10 @@ function randInt(min, max) {
 	return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+function defaults(thing, default) {
+	return thing === undefined ? default : thing;
+}
+
 function param(name) {
 	return decodeURIComponent((new RegExp('[?|&]' + name + '=' +
 		'([^&;]+?)(&|#|;|$)').exec(location.search) || [null, ''])[1].replace(
