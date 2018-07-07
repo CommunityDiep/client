e6048cd6e54ae122fa92718389b04cf8691afb00
haykam821 <24855774+haykam821@users.noreply.github.com>
2018-07-07 02:50:25 -0400

    Some basic menu things

--- src/client.js
+++ src/client.js
@@ -2,5 +2,57 @@ const funcs = require("./functions.js");
 
 const $ = require("jquery-browserify");
 
+const ws = new WebSocket("ws://localhost:8080/");
+
 const canvas = $("#main");
 canvas.contextmenu(event => event.preventDefault());
+
+function resize() {
+	canvas.attr("width", window.innerWidth);
+	canvas.attr("height", window.innerHeight);
+}
+
+window.addEventListener("resize", resize);
+resize();
+
+const loading = $("#loading");
+window.addEventListener("load", () => {
+	loading.css("display", "none");
+});
+
+const inGame = false;
+let isConnected = false;
+
+ws.addEventListener("open", () => {
+	isConnected = true;
+});
+
+const input = $("#textInput");
+
+const ctx = canvas.get(0).getContext("2d");
+function render() {
+	ctx.clearRect(0, 0, canvas.attr("width"), canvas.attr("height"));
+
+	if (inGame) {
+		input.css("display", "none");
+	} else {
+		funcs.grid(ctx, 0, 0, canvas.attr("width"), canvas.attr("height"));
+
+		funcs.drawText(ctx, canvas.attr("width") / 2, 60, "Community", 30);
+		funcs.drawText(ctx, canvas.attr("width") / 2, 150, "Diep.io", 100);
+
+		if (isConnected) {
+			input.css("display", "initial");
+
+			funcs.drawText(ctx, canvas.attr("width") / 2, canvas.attr("height") / 2 - 40, "This is the tale of...", 20);
+			funcs.drawText(ctx, canvas.attr("width") / 2, canvas.attr("height") / 2 + 45, "(press enter to spawn)", 15);
+		} else {
+			input.css("display", "none");
+
+			funcs.drawText(ctx, canvas.attr("width") / 2, canvas.attr("height") / 2, "Connecting...", 50);
+		}
+	}
+
+	window.requestAnimationFrame(render);
+}
+render();
--- src/functions.js
+++ src/functions.js
@@ -1,1 +1,36 @@
-const softStroke = true;
+module.exports.grid = (ctx, x = 0, y = 0, width, height, slotSize = 24, fillColor = "#CDCDCD", lineColor = "#C6C6C6") => {
+	ctx.fillStyle = fillColor === undefined ? "#cdcdcd" : fillColor;
+	ctx.fillRect(x, y, width, height);
+	ctx.save();
+	ctx.translate(x, y);
+	ctx.beginPath();
+	ctx.strokeColor = lineColor;
+	ctx.lineWidth = 1;
+	for (let i = 0; i < width || i < height; i += slotSize) {
+		ctx.moveTo(0, i);
+		ctx.lineTo(width, i);
+		ctx.moveTo(i + (x % slotSize), 0);
+		ctx.lineTo(i + (x % slotSize), height);
+	}
+	ctx.strokeStyle = lineColor;
+	ctx.stroke();
+	ctx.closePath();
+	ctx.restore();
+};
+
+module.exports.drawText = (ctx, x = 0, y = 0, text, size) => {
+	ctx.font = `bold ${size}px Ubuntu`;
+	ctx.lineWidth = 4;
+	ctx.textAlign = "center";
+
+	ctx.save();
+	ctx.translate(x, y);
+
+	ctx.fillStyle = "#333333";
+	ctx.strokeText(text, 0, 0);
+
+	ctx.fillStyle = "white";
+	ctx.fillText(text, 0, 0);
+
+	ctx.restore();
+};
